9b48802db2ab67a26c8981e51aa5ad57e5ca9173
uko-flip <gekomixy@gmail.com>
2020-12-28 19:38:33 +0300

    Update project structure + bugfix

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+static char cpu[30000];
+
+int main(int argc, char **argv) {
+	if(argc < 2) {
+		cerr << "Error: too few arguments" << endl
+		     << "Usage: brainc [brainfuck_file] [flags]" << endl;
+	}
+    vector<char> acc;
+    char ch;
+    ifstream infile(argv[1]);
+    while (infile) {
+        infile.get(ch);
+        acc.push_back(ch);
+    }
+    infile.close();
+    unsigned int j = 0;
+    int brc = 0;
+    for (int i = 0; i < acc.size(); ++i) {
+        if (acc[i] == '>')
+            j++;
+        if (acc[i] == '<')
+            j--;
+        if (acc[i] == '+')
+            cpu[j]++;
+        if (acc[i] == '-')
+            cpu[j]--;
+        if (acc[i] == '.')
+            cout << cpu[j];
+        if (acc[i] == ',')
+            cin >> cpu[j];
+        if (acc[i] == '[') {
+            if (!cpu[j]) {
+                ++brc;
+                while (brc) {
+                    ++i;
+                    if (acc[i] == '[')
+                        ++brc;
+                    if (acc[i] == ']')
+                        --brc;
+                }
+            } else
+                continue;
+        } else if (acc[i] == ']') {
+            if (!cpu[j])
+                continue;
+            else {
+                if (acc[i] == ']')
+                    brc++;
+                while (brc) {
+                    --i;
+                    if (acc[i] == '[')
+                        brc--;
+                    if (acc[i] == ']')
+                        brc++;
+                }
+                --i;
+            }
+        }
+    }
+}
